2e1bd1b16b62e15b7ba5d069e7d214861a8c79ea
VladimirVorobyov <vovabandit1945@gmail.com>
2021-10-24 21:43:22 +0300

    json

--- project/src/const.tsx
+++ project/src/const.tsx
@@ -1,11 +1,11 @@
-export enum AppRoute{
+export enum AppRoute {
   Main = '/',
   SignIn = '/login',
   Favorites = '/favorites',
   Room = '/offer/:id?',
 }
 
-export enum  AuthorizationStatus{
+export enum AuthorizationStatus {
   Auth = 'AUTH',
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
