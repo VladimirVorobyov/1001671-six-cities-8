88fdc998d32890d0e22823e8e6aaa4dcf0b75f95
VladimirVorobyov <vovabandit1945@gmail.com>
2021-10-28 11:40:56 +0300

    lesson1

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -2,7 +2,6 @@ import { applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import cityReducer from './citiesReducer';
 
-
 const store = createStore(cityReducer, applyMiddleware(thunk));
 
 export default store;
